785267713ec32516ac4c53462517963a3ba80577
Rodrigo Naranjo González <naranjogonzalezrodrigo@gmail.com>
2023-11-21 03:13:48 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,4 +1,3 @@
-
 open Parsing;;
 open Lexing;;
 
@@ -6,13 +5,25 @@ open Lambda;;
 open Parser;;
 open Lexer;;
 
+let read_cmd () =
+  let rec in_loop acc =
+    try
+      let line = read_line () in
+      if String.ends_with ~suffix:";;" line then 
+        String.concat " " (List.rev (String.sub line 0 (String.length line - 2) :: acc))
+      else 
+        in_loop (line :: acc)
+    with End_of_file ->
+      String.concat " " (List.rev acc)
+    in in_loop []
+
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
   let rec loop ctx =
     print_string ">> ";
     flush stdout;
     try
-      let tm = s token (from_string (read_line ())) in
+      let tm = s token (from_string (read_cmd ())) in
       let tyTm = typeof ctx tm in
       print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
       loop ctx
@@ -34,4 +45,3 @@ let top_level_loop () =
 
 top_level_loop ()
 ;;
-
